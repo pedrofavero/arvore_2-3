a80d3ac90ebf0cc67c9813b4886123a07601503f
pedrofavero <112567328+pedrofavero@users.noreply.github.com>
2025-10-05 21:28:14 -0300

    operações adicionais, para mais teste

--- Main.java
+++ Main.java
@@ -15,6 +15,36 @@ public class Main {
         arvore.remover(15);
         System.out.print("Árvore após remover 15: ");
         arvore.imprimir();
+
+        // ----------------- ADICIONAl -----------------
+        // arvore.inserir(30);
+        // arvore.inserir(2);
+        // arvore.inserir(8);
+        // arvore.inserir(12);
+        // arvore.inserir(18);
+        // arvore.inserir(22);
+        // arvore.inserir(28);
+        // arvore.inserir(35);
+        // System.out.print("Após inserções adicionais: ");
+        // arvore.imprimir();
+
+        // arvore.remover(5);   
+        // System.out.print("Após remover 5 (folha): ");
+        // arvore.imprimir();
+
+        // arvore.remover(10);   
+        // System.out.print("Após remover 10 (interno): ");
+        // arvore.imprimir();
+
+        // arvore.inserir(0);    
+        // arvore.inserir(100);   
+        // System.out.print("Após inserir extremos {0, 100}: ");
+        // arvore.imprimir();
+
+        // arvore.remover(0);
+        // arvore.remover(100);
+        // System.out.print("Após remover extremos {0, 100}: ");
+        // arvore.imprimir();
     }
     
 }
